c319f8cb163bb4322cbf10b11db6508b5ddfb745
Betageri <sonali.betageri@accenture.com>
2025-07-18 11:11:08 +0530

    Toggle modes(dark/light) and alerts

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+// import About from './Components/About';
+import Navbar from "./Components/Navbar"
+import Textform from "./Components/Textform"
+import Alert from './Components/Alert';
 
 function App() {
+  const [alert, setAlert] = useState(null)
+
+  const showAlert = (message, type)=>{
+    setAlert({
+      msg:message,
+      type:type 
+    })
+    setTimeout(() => {
+      setAlert(null)
+    }, 1500);
+  }
+  const [mode, setMode] = useState('light')
+
+  const togglemode = ()=>{
+    if(mode === 'light'){
+      setMode('dark')
+      document.body.style.backgroundColor = '#3d3d3d'
+      document.body.style.Color = 'white'
+      showAlert("Switched to dark mode","success")
+    }
+    else{
+      setMode('light')
+      document.body.style.backgroundColor = 'white'
+      showAlert("Switched to light mode","success")
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar title="TextUtils" mode={mode} togglemode={togglemode}/>
+      <Alert alert={alert} />
+      <div className='container my-3'>
+        <Textform heading="Enter your text here" mode={mode} showAlert={showAlert} />
+        {/* <About/> */}
+      </div>
+    </>
   );
 }
 
---
+++ src/Components/Textform.js
@@ -0,0 +1,51 @@
+import React, {useState} from 'react'
+
+export default function Textform(props) {
+    const [text, setText] = useState('');
+    const upperCaseClicked = ()=>{
+        let upcaseText = text.toUpperCase()
+        setText(upcaseText)
+        props.showAlert("Converted to Uppercase", "success")
+    }
+    const lowerCaseClicked = ()=>{
+        let upcaseText = text.toLowerCase()
+        setText(upcaseText)
+        props.showAlert("Converted to Lowercase", "success")
+    }
+    const handleOnchange = (event)=>{
+        setText(event.target.value)
+        
+    }
+    const clearTextClicked = ()=>{
+        setText("")
+    }
+    const handleCopy = ()=>{
+        var text = document.getElementById('mybox')
+        text.select()
+        navigator.clipboard.writeText(text.value)
+        props.showAlert("Copied to clipboard",'success')
+    }
+    let wordCount = text.split(/\s+/).filter(word => word !== '').length;
+    return (
+        <>
+        <div className='container'>
+            <div className="mb-3" style={{color: props.mode==='light'?'black':'white'}}>
+                <h2>{props.heading}</h2>
+                <textarea className="form-control" value={text} onChange={handleOnchange}id="mybox" rows="8" style={{backgroundColor: props.mode==='light'?'white': '#3d3d3d', color: props.mode==='light'?'black':'white'}}></textarea>
+            </div>
+            <button className="btn btn-primary mx-1" onClick={upperCaseClicked}>Convert to uppercase</button>
+            <button className="btn btn-primary mx-1" onClick={lowerCaseClicked}>Convert to lowercase</button>
+            <button className="btn btn-primary mx-1" onClick={handleCopy}>Copy</button>
+            <button className="btn btn-primary mx-1" onClick={clearTextClicked}>Clear text</button>
+        </div>
+        <div className="container" style={{color: props.mode==='light'?'black':'white'}}>
+            <h1>Your text summary</h1>
+            <p>{wordCount} words and {text.length} characters</p>
+            <p> {Math.round(0.008 * wordCount)} minutes read</p>
+            <h2>Preview</h2>
+            <p>{text}</p>
+        </div>
+        
+        </>
+    )
+}
